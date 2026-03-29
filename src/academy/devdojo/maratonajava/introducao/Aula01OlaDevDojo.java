0147723014393201d4f1ce1a2c0a351ef3634ab9
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-03-28 23:21:49 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+	/**Isso é um comentario em <b> JavaDoc </b>**/
+	public static void main(String[] args) {
+		System.out.println("Olá, Mundo!");
+		}
+
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+	public static void main(String[] args) {
+		
+		int age = (int) 10000000000L;
+		long numeroGrande = 10000L;
+		double salarioDouble = 2000.0D;
+		float salarioFloat = 2500.0F;
+		byte idadeByte = 127;
+		short idadeShort = 32000;
+		boolean verdadeiro = true;
+		boolean falso = false;
+		char caractere = 87;
+		
+		
+		System.out.println("a idade é "+age+ " anos");
+		System.out.println(caractere);
+		System.out.println(verdadeiro);
+		System.out.println(falso);
+		System.out.println(idadeShort);
+		System.out.println(idadeByte);
+		System.out.println(salarioFloat);
+		System.out.println(salarioDouble);
+		System.out.println(numeroGrande);
+	}
+}
