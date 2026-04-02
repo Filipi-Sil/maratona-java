fc5afa5f86a19d5093a89b81385c8724e70a77f6
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-01 22:29:20 -0300

    Aula 15

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+	public static void main(String[] args) {
+		double num1 = 10;
+		int num2 = 20;
+		double resul = num1 / num2;
+		System.out.println(resul);
+	}
+}
