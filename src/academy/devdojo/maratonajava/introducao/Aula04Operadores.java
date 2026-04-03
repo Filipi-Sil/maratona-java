015e930f21e40f312fd1bffa61cdedb4b8d134ef
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-03 17:00:27 -0300

    Aula 18

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -3,19 +3,19 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula04Operadores {
 	public static void main(String[] args) {
 		// + - / *
-		System.out.println("Operadores Aritiméticos");
+		System.out.println("-Operadores Aritiméticos-");
 		double num1 = 10;
 		int num2 = 20;
 		double resul = num1 / num2;
 		System.out.println(resul);
 		
 		// %
-		System.out.println("Operador Resto");
+		System.out.println("-Operador Resto-");
 		int resto = 8 % 2;
 		System.out.println(resto);
 		
 		// < > <= >= == !=
-		System.out.println("Operadores Relacionais");;
+		System.out.println("-Operadores Relacionais-");;
 		boolean isDezMaiorQueVinte = 10 > 20;
 		boolean isDezMenorQueVinte = 10 < 20;
 		boolean isDezIgualVinte = 10 == 20;
@@ -26,12 +26,20 @@ public class Aula04Operadores {
 		System.out.println(isDezDiferenteVinte);
 		
 		// && || !
-		System.out.println("Operadores Logicos");
+		System.out.println("-Operadores Logicos-");
 		int idade = 29;
 		float salario = 3400F;
 		boolean isDentroDaLeiMaiorQue30 = idade >= 30 && salario >= 4612;
 		boolean isDentroDaLeiMenorQue30 = idade < 30 && salario >= 3381;
 		System.out.println("Você está dentro da lei maior que 30? "+isDentroDaLeiMaiorQue30);
 		System.out.println("Você está dentro da lei menor que 30? "+isDentroDaLeiMenorQue30);
+		
+		double valorTotalContaCorrente = 200;
+		double valorTotalContaPoupanca = 10000;
+		float valorPlaystation = 5000;
+		boolean isPlaystationCompravel = valorTotalContaCorrente >= valorPlaystation || valorTotalContaPoupanca >= valorPlaystation;
+		
+		System.out.println("Você pode comprar um Playstation? "+isPlaystationCompravel);
+		
 	}
 }
