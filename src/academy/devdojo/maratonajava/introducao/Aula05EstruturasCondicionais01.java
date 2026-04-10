15e73458516dd72d9f66f9e50fb66e3fcc028a9f
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-09 23:04:25 -0300

    Aula 21

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,19 +1,17 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
 	public static void main(String[] args) {
-		System.out.println("--Estrutura IF--");
 		
 		int idade = 10;
 		boolean isAutorizadoComprarBebida = idade >= 18;
 		
 		 if(isAutorizadoComprarBebida) {
 			 System.out.println("Autorizado a comprar bebida alcólica");
-		 } 
-		 
-		 if(!isAutorizadoComprarBebida) {
+		 } else {
 			 System.out.println("Não autorizado a comprar bebida alcólica");
 		 }
+		 
 		 System.out.println("Fora do if");
 	}
 
---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais02 {
+
+	public static void main(String[] args) {
+		
+		//idade < 15(infantil) , idade >= 15 && idade < 18(juvenil), idade >= 18 (adulto)
+		int idade = 17;
+		String categoria;
+		
+		if(idade < 15) {
+			categoria = "Categoria Infantil";
+		} else if(idade >= 15 && idade < 18) {
+			categoria = "Categoria Juvenil";
+		}else {
+			categoria = "Categoria Adulto";
+		}
+		System.out.println(categoria);
+		
+	}
+
+}
