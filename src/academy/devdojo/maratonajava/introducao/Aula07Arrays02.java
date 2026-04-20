70403fb791440c0cc2ca74a44cddd26c97d2aa40
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-20 20:08:41 -0300

    Aula 34

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -4,12 +4,23 @@ public class Aula07Arrays02 {
 	
 
 	public static void main(String[] args) {
-		//
+		//byte, short, int, float, long, double 0
+		//char '\u0000' == ' '
+		//boolean false
+		//String null
 		
-		String [] idades = new String[3];
+		String [] nomes = new String[4];
+		nomes = new String[8];
 		
-		System.out.println(idades[0]);
-		System.out.println(idades[1]);
+		nomes[0] = "Sanji";
+		nomes[1] = "Shikamaru";
+		nomes[2] = "Tomioka";
+		nomes[3] = "Crocodile";
+		
+		
+		for(int i = 0; i < nomes.length; i++) {
+			System.out.println(nomes[i]);
+		}
 		
 
 	}
