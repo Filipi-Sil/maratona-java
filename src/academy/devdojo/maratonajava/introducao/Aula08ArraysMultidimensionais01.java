e5a6007505210dbca4ba542f430f9c93049f50d4
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-21 00:10:47 -0300

    Aula 35 && 36

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+
+	public static void main(String[] args) {
+		// 1,2,3,4 Meses
+		//31,28,31,30 Dias
+		int[][] dias = new int [3][3];
+		
+		dias [0][0] = 31;
+		dias [0][1] = 28;
+		dias [0][2] = 31;
+		
+		dias [1][0] = 30;
+		dias [1][1] = 31;
+		dias [1][2] = 30;
+		
+		dias [2][0] = 31;
+		dias [2][1] = 31;
+		dias [2][2] = 30;
+		
+		
+		for(int i = 0; i< dias.length; i++) {
+			for(int z = 0; z < dias[0].length; z++) {
+				System.out.println(dias[i][z]);
+			}
+		}
+			
+		
+
+	}
+
+}
