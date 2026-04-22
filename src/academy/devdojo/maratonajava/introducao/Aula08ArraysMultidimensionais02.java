36fc96600d0c0fe994ab66fb39f35f5a9da24084
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-22 00:04:11 -0300

    Aula 38

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais02 {
+
+	public static void main(String[] args) {
+		int[][] arrayInt = new int[3][];
+		
+		arrayInt[0] = new int[]{1,2};
+		arrayInt[1] = new int[] {1,2,3};
+		arrayInt[2] = new int[]{1,2,3,4,5,6};
+		
+		int[][] arrayInt2 = {{2,1}, {3,2,1}, {6,5,4,3,2,1}};
+		
+		for(int[] arrayBase:arrayInt2) {
+			System.out.println("\n--------");
+			for(int num:arrayBase) {
+				System.out.print(num+" ");		
+			}
+		}
+	}
+
+}
