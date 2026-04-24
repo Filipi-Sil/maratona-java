903ecae27b9e09792107a891190a598b26da5cdd
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-24 17:15:38 -0300

    Aula 40

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Estudante {
+	public String nome = "zoro";
+	public int idade = 23;
+	public char sexo = 'F';
+}
