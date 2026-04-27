8ba79e6c0b99f3c593f999a191997932c8f2a8a5
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-27 02:21:32 -0300

    Aula 45

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+
+	public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		int bl = 10;
+		calculadora.multiplicaDoisNumeros(bl, 9);
+
+	}
+
+}
