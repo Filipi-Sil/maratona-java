c8ffeffedcb24825b5faaeffbeed4f72e41e0e6e
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-04-26 21:36:16 -0300

    Aula 43

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -6,12 +6,17 @@ public class CarroTest01 {
 	public static void main(String[] args) {
 	Carro carro1 = new Carro();
 	Carro carro2 = new Carro();
+	
 	carro1.nome = "Chevrolet Onix";
 	carro1.modelo = "Sedan";
 	carro1.ano = 2020;
+	
 	carro2.nome = "Ford Focus";
 	carro2.modelo = "Hatch";
 	carro2.ano = 2013;
+	
+	carro1 = carro2;
+	
 	System.out.println("O primeiro carro é um "+carro1.nome+" do modelo "+carro1.modelo+" do ano de "+carro1.ano);
 	System.out.println("O segundo carro é um "+carro2.nome+" do modelo "+carro2.modelo+" do ano de "+carro2.ano);
 
