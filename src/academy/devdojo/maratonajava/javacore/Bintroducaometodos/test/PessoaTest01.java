737804f389e7e4c4278e0bfeae6d911bd4e54182
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-05-07 22:50:28 -0300

    Aula 54

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+
+	public static void main(String[] args) {
+		Pessoa pessoa = new Pessoa();
+		//pessoa.nome = "Jiraya";
+		pessoa.setNome("jiraya");
+		pessoa.setIdade(67);
+	//	pessoa.imprime();
+		System.out.println(pessoa.getNome());
+		System.out.println(pessoa.getIdade());
+	}
+	
+
+}
