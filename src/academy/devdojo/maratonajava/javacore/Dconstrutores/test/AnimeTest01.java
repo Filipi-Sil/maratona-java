bb4b54446426ee1b6bd2887cefb497734f46d4e9
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-05-20 18:31:02 -0300

    Aula 59

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,11 +4,7 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
 	public static void main(String[] args) {
-		Anime anime = new Anime("Grand Blue", "TV", 24,"Comédia");
-		new Anime();
-		//anime.init("Grand Blue", "TV", 24,"Comédia");
-		Anime anime2 = new Anime();
+		Anime anime = new Anime("Grand Blue", "TV", 24,"Comédia", "Toei");
 		anime.imprime();
-		anime2.imprime();
 	}
 }
