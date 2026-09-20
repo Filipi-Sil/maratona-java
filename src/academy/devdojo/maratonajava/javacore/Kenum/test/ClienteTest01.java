3c1bd18d918bfeb2973e210f2902109d2821565a
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-09-20 20:02:34 -0300

    Aula 84

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -14,7 +14,7 @@ public class ClienteTest01 {
 		System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
 		TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
 		System.out.println(tipoCliente);
-		TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
+		TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Jurídica");
 		System.out.println(tipoCliente2);
 	}
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio.*;
+
+public class FuncionarioTest01 {
+	public static void main(String[] args) {
+		Gerente gerente = new Gerente("Nami", 5000);
+		Desenvolvedor desenvolvedor = new Desenvolvedor("Franky", 3000);
+		System.out.println(gerente);
+		System.out.println(desenvolvedor);
+	}
+}
