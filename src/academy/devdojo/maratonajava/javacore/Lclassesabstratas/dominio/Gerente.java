8f07c5a8ef47def85ba7116d7d2b8d53dd5eb033
Filipi Silvestrini Botelho <filipisilvestrinibotelho@gmail.com>
2026-09-24 16:16:14 -0300

    Aula 85

--- src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Gerente.java
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Gerente.java
@@ -5,6 +5,11 @@ public class Gerente extends Funcionario{
 	public Gerente(String nome, double salario) {
 		super(nome, salario);
 	}
+
+	@Override
+	public void calculaBonus() {
+		this.salario = this.salario + this.salario * 0.2;
+	}
 	
 	@Override
 	public String toString() {
